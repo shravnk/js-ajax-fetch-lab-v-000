905a19b2165c18166f8bcdcbe211d66a915ab88a
Brendan O'Dea <brendan.ode4@gmail.com>
2018-05-21 22:57:38 +0000

    Saving first pass

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function getIssues() {
+
 }
 
 function showIssues(json) {
@@ -8,15 +9,23 @@ function createIssue() {
 }
 
 function showResults(json) {
+  $("#results").html(`<a href="${json.html_url}">Visit Fork Page</a>`)
 }
 
 function forkRepo() {
+  const token = getToken()
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+  const link = `https://api.github.com/repos/${repo}/forks`
+  fetch(link, {
+    method: 'post',
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => res.json()).then(json => showResults(json));
 }
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return ''
+  return '478051c5bd3d221b5e9b8210d0b499d2ca57c938'
 }
